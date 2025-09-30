b0a5ab469d6f8bf76bf053de1e47951a1f3e6497
andreslmortensen <lorenzomortensen@gmail.com>
2025-09-29 23:30:09 -0300

    Add files via upload

---
+++ Sistema-Hospitalario-ConLombok/SistemaHospital-main/src/main/java/org/jcr/entidades/Matricula.java
@@ -0,0 +1,29 @@
+package org.jcr.entidades;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Matricula implements Serializable {
+
+    @EqualsAndHashCode.Include
+    private final String numero;
+
+    public Matricula(String numero) {
+        this.numero = validarMatricula(numero);
+    }
+
+    private String validarMatricula(String numero) {
+        Objects.requireNonNull(numero, "El número de matrícula no puede ser nulo");
+        if (!numero.matches("MP-\\d{4,6}")) {
+            throw new IllegalArgumentException("Formato de matrícula inválido. Debe ser como MP-12345");
+        }
+        return numero;
+    }
+}
